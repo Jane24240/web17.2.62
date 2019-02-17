74c3e3f124d98d995ea613f25ea8c642b70a4f98
jane24240 <kanorut.b@ku.th>
2019-02-17 12:05:06 +0700

    jane

--- Desktop/web17.2/web2/src/app/form/form.component.ts
+++ Desktop/web17.2/web2/src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { User } from '../user';
 
 @Component({
   selector: 'app-form',
@@ -20,5 +21,10 @@ export class FormComponent implements OnInit {
      lastName: ['']
    })
   }
-
+  onSubmit(from: FormGroup){
+    const{firstName,lastName}=from.value;
+    const user = new User(firstName,lastName);
+    console.log(User);
+    
+  }
 }
